02b8b0a6cdcb4eeb20c80279c16924d1472186a1
llt <418797644@qq.com>
2024-06-29 00:04:35 +0800

    update

---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+/**
+ * https://nextra.site/docs/docs-theme/theme-configuration
+ */
+const themeConfig = {
+  logo: <span>Notes Docs</span>,
+  project: {
+    link: 'https://github.com/LangLiGeLangLLT/notes-docs-app',
+  },
+  docsRepositoryBase: 'https://github.com/LangLiGeLangLLT/notes-docs-app',
+  feedback: {
+    content: null,
+  },
+}
+
+export default themeConfig
